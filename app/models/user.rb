f0c01bff975bb01284b61e2d5a56e287a95798b2
harujamu <tnagisa1220@gmail.com>
2022-07-10 07:46:47 +0000

    [Save]Bookers2-部分テンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ApplicationRecord
       profile_image.variant(resize_to_limit:[width,height]).processed
     end
  
- validates :title, presence: true
- validates :body, presence: true
- validates :profile_image, presence: true
+ validates :name, uniqueness: true, length:{in:2..20}
+ validates :introduction, length:{maximum:50}
 end
