c7ca8d2f7a6a297067b458aff8c835d15adacbe6
harujamu <tnagisa1220@gmail.com>
2022-07-10 09:21:10 +0000

    [Save]Bookers2-部分テンプレート解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,15 @@ class UsersController < ApplicationController
   end
   
   def index
+    @user = current_user
     @users = User.all
+    @book = Book.new
   end
   
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
